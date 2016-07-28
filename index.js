6ba9464fb0b307f645c1ca7704a8d7d940101f8e
troylelandshields <troy.leland.shields@gmail.com>
2016-07-27 20:20:54 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// //require the Twilio module and create a REST client
+// var client = require('twilio')('AC557fcb165d86419458c2692716202b92', 'bd2a9a5491aea94fd2d5083bf3062f96');
+
+
+// //Place a phone call, and respond with TwiML instructions from the given URL
+// client.makeCall({
+
+//     to:'+16515556677', // Any number Twilio can call
+//     from: '+14506667788', // A number you bought from Twilio and can use for outbound communication
+//     url: '' // A URL that produces an XML document (TwiML) which contains instructions for the call
+
+// }, function(err, responseData) {
+
+//     //executed when the call has been initiated.
+//     console.log(responseData.from); // outputs "+14506667788"
+
+// });
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
