6d397be20a31fba77603c87fbdfd305949e97f6b
troylelandshields <troy.leland.shields@gmail.com>
2016-07-27 20:36:49 -0600

    trying to add a twiml endpoint

--- index.js
+++ index.js
@@ -21,18 +21,33 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+
+//Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({ "error": message });
+}
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.post("/phonecall/incoming", function (req, res) {
+    var phoneCall = req.body;
 
+    res.send(`<Response>
+<Say voice="alice">
+Fk off.
+</Say>`);
+});
 
